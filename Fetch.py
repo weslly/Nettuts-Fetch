dbbadc582c93e4058789fcab08e8ac256104653e
Weslly Honorato <weslly.honorato@gmail.com>
2011-12-28 16:05:05 -0200

    first commit

---
+++ Fetch.py
@@ -0,0 +1,39 @@
+import sublime
+import sublime_plugin
+import urllib
+import urllib2
+
+
+class FetchCommand(sublime_plugin.WindowCommand):
+    fetchList = []
+    def __init__(self, *args, **kwargs):
+        super(FetchCommand, self).__init__(*args, **kwargs)
+
+
+    def run(self):
+        s = sublime.load_settings('Fetch.sublime-settings')
+        files = s.get('files')
+        self.fetchList = []
+        for name, url in files.iteritems():
+            self.fetchList.append([name, url])
+            
+        self.window.show_quick_panel(self.fetchList , self.callback, sublime.MONOSPACE_FONT)
+
+    def callback(self, index):
+        if (index > -1):
+            url = self.fetchList[index][1]
+            self.window.active_view().run_command("fetch_insert_file", {"url": url})
+
+class FetchInsertFileCommand(sublime_plugin.TextCommand):
+    def run(self, edit, url):
+        try:
+            request = urllib2.Request(url)
+            http_file = urllib2.urlopen(request, timeout=5)
+            self.result = http_file.read()
+
+        except (urllib2.URLError) as (e):
+            err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+            sublime.error_message(err)
+
+        for region in self.view.sel():
+            self.view.replace(edit, region, self.result)

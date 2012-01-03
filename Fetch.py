5e6b149a3b7122de10f2acecacf77763ce008049
Weslly Honorato <weslly.honorato@gmail.com>
2012-01-03 02:07:36 -0200

    pre-populate settings on init

--- Fetch.py
+++ Fetch.py
@@ -19,8 +19,18 @@ class FetchCommand(sublime_plugin.WindowCommand):
     s = None
     packageUrl = None
 
+    filesPlaceholder = {"jquery" : "http://code.jquery.com/jquery.min.js"}
+    packagesPlaceholder = {"html5-boilerplate" : "http://github.com/h5bp/html5-boilerplate/zipball/v2.0stripped"}
+
     def __init__(self, *args, **kwargs):
         super(FetchCommand, self).__init__(*args, **kwargs)
+
+        s = sublime.load_settings('Fetch.sublime-settings')
+        if not s.has('packages'):
+            s.set('packages', self.packagesPlaceholder)
+        if not s.has('files'):
+            s.set('files', self.filesPlaceholder)
+        sublime.save_settings('Fetch.sublime-settings')
 
 
     def run(self):
@@ -41,7 +51,7 @@ class FetchCommand(sublime_plugin.WindowCommand):
     def list_packages(self):
         packages = self.s.get('packages')
         if not packages:
-            self.s.set('packages', {"html5-boilerplate" : "http://github.com/h5bp/html5-boilerplate/zipball/v2.0stripped"})
+            self.s.set('packages', self.packagesPlaceholder)
             sublime.save_settings('Fetch.sublime-settings')
             packages = self.s.get('packages')
 
@@ -76,7 +86,7 @@ class FetchCommand(sublime_plugin.WindowCommand):
         files = self.s.get('files')
 
         if not files:
-            self.s.set('files', {"jquery" : "http://code.jquery.com/jquery.min.js"})
+            self.s.set('files', self.filesPlaceholder)
             sublime.save_settings('Fetch.sublime-settings')
             files = self.s.get('files')
 
